d1bbaed9ef86a45b7a59f51fd8741cc6cbd720c4
Alan Wu <alanwuandriod@gmail.com>
2024-03-12 23:08:00 +1300

    Add spheres.

--- src/demo/createApp.js
+++ src/demo/createApp.js
@@ -2,16 +2,40 @@ import './index.css';
 import Zinc from "zincjs";
 import { PhyZinc } from "../phyZinc.js"
 
+
+const addSpheres = (phyZinc, radius, counts, dimension, position) => {
+  const offset = [
+    position[0] - dimension[0] / 2.0,
+    position[1] - dimension[1] / 2.0,
+  ]
+  const increment = [
+    dimension[0] / (counts[0] + 1),
+    dimension[1] / (counts[1] + 1),
+  ];
+  for (let i = 0;  i < counts[0]; i++) {
+    const x = offset[0] + increment[0] * ( i + 1 );
+    for (let j = 0; j < counts[1]; j++) {
+      const y = offset[1] + increment[1] * ( j + 1 );
+      phyZinc.addSphere([x, y, position[2] + radius], radius, 16, 16);
+    }
+  }
+}
+
 export async function createApp() {
   const phyZinc = new PhyZinc();
-  console.log("Initialise rapier")
   await phyZinc.initialise();
-  console.log("here")
+  console.log("Initialised rapier")
   const renderElt = document.getElementById('root');
   const renderer = new Zinc.Renderer(renderElt, window);
-  phyZinc.attach(renderer);
   Zinc.defaultMaterialColor = 0xFFFF9C;
+  phyZinc.attach(renderer);
+  phyZinc.startNewScene("test");
+  phyZinc.setGravity(-98.1);
+  const dimension = [2000, 2000];
+  const position = [0, 0, -500];
+  phyZinc.addFloor(position, /*dimenstion*/dimension);
+  addSpheres(phyZinc, /*radius*/30, [10, 10], [700, 700], position);
   const metaURL = "/body_metadata.json"
-  phyZinc.importZincMetadata("test", metaURL);
+  phyZinc.importZincMetadata(metaURL);
   return renderer;
 }
--- src/phyZinc.js
+++ src/phyZinc.js
@@ -12,6 +12,18 @@ const PhyZinc = function() {
     let gravity = -9.81;
     const objects = [];
 
+    this.setGravity = g => {
+        gravity = g;
+    }
+
+    this.getPhysicsWorld = () => {
+        if (!this.physicsWorld) {
+            gravity = new THREE.Vector3(0.0, 0.0, gravity);
+            this.physicsWorld = new this.rapier.World(gravity);
+        }
+        return this.physicsWorld;
+    }
+
     this.initialise = async () => {
         if (!this.rapier) {
             this.rapier = await getRapier();
@@ -19,7 +31,70 @@ const PhyZinc = function() {
         return this.rapier;
     }
 
-    this.addPhysics = (zincObject) => {
+    this.addGeometry = (geometry, material, name) => {
+        const scene = this.renderer.getCurrentScene();
+        const zincObject = new Zinc.Geometry();
+        zincObject.setName(name);
+        zincObject.createMesh(
+            geometry,
+            material,
+            {
+                opacity: 1.0,
+                localTimeEnabled: false,
+                localMorphColour: false
+
+            }
+        );
+        scene.addZincObject(zincObject);
+        return zincObject;
+    }
+
+    this.addSphere = (position, radius, widthSegments, heightSegments) => {
+        if (this.rapier) {
+            const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+            const material = new THREE.MeshPhongMaterial({
+                color: new THREE.Color("rgb(255, 215, 0)")
+            });
+            //geometry.translate(position[0], position[1], position[2]);
+            console.log(geometry)
+            const zincObject = this.addGeometry(geometry, material, "balls");
+            if (zincObject) {
+                const world = this.getPhysicsWorld();
+                const rbDesc = this.rapier.RigidBodyDesc.dynamic()
+                    .setTranslation(position[0], position[1], position[2])
+                    .setLinearDamping(0.1);
+                const rigidBody = world.createRigidBody(rbDesc);
+                const collider = this.rapier.ColliderDesc.ball(radius)
+                    .setFriction(0.1)
+                    .setFrictionCombineRule(this.rapier.CoefficientCombineRule.Max)
+                    // .setTranslation(0, 0, 0)
+                    .setRestitution(0.6)
+                    .setRestitutionCombineRule(this.rapier.CoefficientCombineRule.Max);
+                this.addPhysics(zincObject, true, true, false, collider, rigidBody);
+            }
+            return zincObject;
+        } else {
+            console.error("Physics engine is not ready yet.");
+        }
+    }
+
+    this.addFloor = (position, dimension) => {
+        if (this.rapier) {
+            const geometry = new THREE.PlaneGeometry(dimension[0], dimension[1]);
+            const material = new THREE.MeshPhongMaterial({
+                color: new THREE.Color("rgb(124, 252, 0)")
+            });
+            geometry.translate(position[0], position[1], position[2]);
+            const zincObject = this.addGeometry(geometry, material, "floor");
+            this.addPhysics(zincObject, false, false, false, undefined, undefined);
+            return zincObject;
+        } else {
+            console.error("Physics engine is not ready yet.");
+        }
+    } 
+
+    this.addPhysics = (zincObject, dynamic, translation, rotation,
+        colliderIn, rigidBodyIn) => {
         if (this.rapier) {
             if (zincObject && zincObject.isGeometry) {
                 const morph = zincObject.getMorph();
@@ -27,14 +102,25 @@ const PhyZinc = function() {
                 const vertices = geometry.attributes.position.array;
                 const indices = geometry.index.array;
                 try {
-                    let collider = this.rapier.ColliderDesc.trimesh(
-                        vertices, indices);
-                    zincObject.getMorph().matrixAutoUpdate = true;
+                    const world = this.getPhysicsWorld();
+                    let collider = colliderIn ? 
+                        colliderIn : this.rapier.ColliderDesc.trimesh(vertices, indices);
                     zincObject.collider = collider;
-                    zincObject.rigidBody = this.physicsWorld.createRigidBody(
-                        this.rapier.RigidBodyDesc.dynamic());
-                    zincObject.worldCollider = this.physicsWorld.createCollider(
-                        collider, zincObject.rigidBody);
+                    let rigidBody = rigidBodyIn;
+                    if (dynamic) {
+                        zincObject.getMorph().matrixAutoUpdate = true;
+                        zincObject.rigidBody = rigidBody ? 
+                            rigidBody : world.createRigidBody(
+                                this.rapier.RigidBodyDesc.dynamic());
+                    } else {
+                        zincObject.rigidBody = rigidBody?
+                            rigidBody: world.createRigidBody(
+                                this.rapier.RigidBodyDesc.fixed());
+                    }
+                    zincObject.worldCollider = world.createCollider(
+                        zincObject.collider, zincObject.rigidBody);
+                    zincObject.physicsT = translation;
+                    zincObject.physicsR = rotation;
                     objects.push(zincObject);
                 }
                 catch {
@@ -46,12 +132,14 @@ const PhyZinc = function() {
     
     const objectAddedCallback = () => {
         return (zincObject) => {
-            this.addPhysics(zincObject);
+            console.log(zincObject.getBoundingBox())
+            this.addPhysics(zincObject, true, true, true, undefined, undefined);
         }
     }
       
     const downloadCompletedCallback = () => {
         return () => {
+            this.renderer.addPreRenderCallbackFunction(updatePhysicalWorld());
             this.renderer.playAnimation = true;
             this.renderer.animate();
         }
@@ -59,29 +147,38 @@ const PhyZinc = function() {
 
     const updatePhysicalWorld = () => {
         return () => {
-            this.physicsWorld?.step();
-            objects.forEach(target => {
-                if (target.isZincObject) {
-                    const t = target.rigidBody.translation();
-                    const morph = target.getMorph()
-                    morph.position.set(t.x, t.y, t.z);
-                }
-            });
+            if (this.physicsWorld) {
+                this.physicsWorld.step();
+                objects.forEach(target => {
+                    if (target.isZincObject) {
+                        const morph = target.getMorph();
+                        if (target.physicsT) {
+                            const t = target.rigidBody.translation();
+                            morph.position.set(t.x, t.y, t.z);
+                        }
+                        if (target.physicsR) {
+                            const r = target.rigidBody.rotation();
+                            morph.quaternion.set(r.x, r.y, r.z, r.w);
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    this.startNewScene = sceneName => {
+        if (this.renderer) {
+            const scene = this.renderer.createScene(sceneName);
+            this.renderer.setCurrentScene(scene);
         }
     }
       
-    this.importZincMetadata = (sceneName, url) => {
+    this.importZincMetadata = (url) => {
         if (this.isReady()) {
             if (this.renderer) {
-                if (!this.physicalWorld) {
-                    gravity = new THREE.Vector3(0.0, 0.0, gravity);
-                    this.physicsWorld = new this.rapier.World(gravity);
-                }
-                const scene = this.renderer.createScene(sceneName);
-                this.renderer.setCurrentScene(scene);
+                const scene = this.renderer.getCurrentScene();
                 scene.loadMetadataURL(url, objectAddedCallback(),
                     downloadCompletedCallback());
-                this.renderer.addPreRenderCallbackFunction(updatePhysicalWorld());
             }
         } else {
             console.error("Physics engine is not ready yet.")
